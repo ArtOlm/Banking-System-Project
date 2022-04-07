ac41db3757b1fd77f322a7fd2fc5315e926158e2
Jaehyeon Park <jpark8@miners.utep.edu>
2022-04-07 12:30:16 -0600

    JunitTest updated

--- JUnit_Test/TransactionsTest.java
+++ JUnit_Test/TransactionsTest.java
@@ -1,13 +1,13 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class TransactionsTest {
     Transactions transactionHandler = Transactions.getInstance();
     @Test
     void userDeposit1() {
-        boolean didNotThrow = true;
+        boolean testResult = true;
         Customer cust = new Customer();
         cust.setCheck(new Checking());
         cust.getCheck().setBalance(100);
@@ -15,46 +15,66 @@ class TransactionsTest {
         try {
             transactionHandler.userDeposit(cust, "Checking", 30);
         }catch (TransactionException e){
-                didNotThrow = false;
+                testResult = false;
         }
-        Assertions.assertTrue(didNotThrow);
+        Assertions.assertTrue(testResult);
     }
     @Test
     void userDeposit2() {
+        boolean testResult = true;
         Customer cust = new Customer();
         cust.setCheck(new Checking());
         //user enters a bad value
-        //Assertions.assertFalse(transactionHandler.userDeposit(cust,"Savings",-100));
+        try {
+            transactionHandler.userDeposit(cust,"Savings",-100);
+        }catch (TransactionException e){
+            testResult = false;
+        }
+        Assertions.assertFalse(testResult);
     }
 
     @Test
     void accountTransfer1() {
+        boolean testResult = true;
         Customer cust = new Customer();
         cust.setCheck(new Checking());
         cust.getCheck().setBalance(100);
         cust.setSave(new Savings());
         cust.getSave().setBalance(30);
         //a successful transaction happens
-        //Assertions.assertTrue(transactionHandler.accountTransfer(cust,"Checking","Savings",50));
+        try {
+            transactionHandler.accountTransfer(cust, "Checking", "Savings", 50.00);
+        }catch (TransactionException e){
+            testResult = false;
+        }
+        Assertions.assertTrue(testResult);
     }
     @Test
     void accountTransfer2() {
+        boolean testResult = true;
         Customer cust = new Customer();
         cust.setCheck(new Checking());
         cust.getCheck().setBalance(100);
         cust.setSave(new Savings());
         cust.getSave().setBalance(30);
         //transaction fails do to lack of funds
-        //Assertions.assertFalse(transactionHandler.accountTransfer(cust,"Checking","Savings",1000));
+        try {
+            transactionHandler.accountTransfer(cust, "Checking", "Savings", 1000000);
+        } catch (TransactionException e){
+            testResult = false;
+        }
+        Assertions.assertFalse(testResult);
     }
 
     @Test
     void transactionToOther1() {
+        boolean testResult = true;
         Customer cust1 = new Customer();
         cust1.setCheck(new Checking());
         cust1.getCheck().setBalance(100);
         cust1.setSave(new Savings());
         cust1.getSave().setBalance(30);
+
         Customer cust2 = new Customer();
         cust2.setCheck(new Checking());
         cust2.getCheck().setBalance(1);
@@ -63,21 +83,33 @@ class TransactionsTest {
         cust1.setID(1);
         cust2.setID(2);
         //transaction is successful
-        //Assertions.assertTrue(transactionHandler.transactionToOther(cust1,cust2,"Checking","Checking",50));
+        try {
+            transactionHandler.transactionToOther(cust1, cust2, "Checking", "Savings", 100);
+        } catch (TransactionException e) {
+            testResult = false;
+        }
+        Assertions.assertTrue(testResult);
     }
     @Test
     void transactionToOther2() {
+        boolean testResult = true;
         Customer cust1 = new Customer();
         cust1.setCheck(new Checking());
         cust1.getCheck().setBalance(100);
         cust1.setSave(new Savings());
         cust1.getSave().setBalance(30);
+
         Customer cust2 = new Customer();
         cust2.setCheck(new Checking());
         cust2.getCheck().setBalance(1);
         cust2.setSave(new Savings());
         cust2.getSave().setBalance(10000);
         //the transaction fails due to insufficient funds
-        //Assertions.assertFalse(transactionHandler.transactionToOther(cust2,cust1,"Checking","Checking",50));
+        try {
+            transactionHandler.transactionToOther(cust1, cust2, "checking", "Savings", 1000000);
+        } catch (TransactionException e) {
+            testResult = false;
+        }
+        Assertions.assertFalse(testResult);
     }
 }
